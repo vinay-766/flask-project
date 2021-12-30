ff24ab590e59b0d9e75ed77299eb9d5ac5593cce
master <gargvinay766@gmail.com>
2021-12-30 21:48:24 +0530

    third route added

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import *
-
+import mysql.connector
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Vinay@0411"
+)
+cur = mydb.cursor()
 
 app = Flask(__name__)
 
@@ -12,6 +18,11 @@ def home():
 @app.route("/login")
 def login():
     return render_template("form.html")
+
+
+@app.route("/user")
+def user():
+    
 
 
 if __name__ == "__main__":
